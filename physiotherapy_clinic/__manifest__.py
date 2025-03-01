f25178e92c4a297a569dc3d9d576b09d908b7487
dlb002 <lbdani96@gmail.com>
2025-03-01 23:11:38 +0100

    Update __manifest__.py

--- physiotherapy_clinic/__manifest__.py
+++ physiotherapy_clinic/__manifest__.py
@@ -30,5 +30,4 @@ Clínica de Fisioterapia""",
     'icon': 'static/description/icon.png',  # Icono
     'images': ['static/description/thumbnail.png'],  # Imagen de portada
     'license': 'LGPL-3',
-    'price': 49.95,
 }
